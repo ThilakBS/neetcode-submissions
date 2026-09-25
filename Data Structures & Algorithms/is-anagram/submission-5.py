961e80ff84a3293fe45642892b31aac5561537f6
Thilak Balaji Sharmila <122073546+ThilakBS@users.noreply.github.com>
2026-09-25 16:56:19 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counters = Counter(s)
+        countert = Counter(t)
+        if counters == countert:
+            return True
+        else:
+            return False
+
